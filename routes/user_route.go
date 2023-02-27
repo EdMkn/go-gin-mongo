b534588cd5ee754fe91f9ec4b3ae78ead297fc6e
EdMkn <nmanyim@rocketmail.com>
2023-02-27 12:57:51 +0100

    changement noms de route

--- routes/user_route.go
+++ routes/user_route.go
@@ -8,9 +8,9 @@ import (
 
 func UserRoute(router *gin.Engine) {
 	//All routes related to users comes here
-	router.POST("/user", controllers.CreateUser())
-	router.GET("/user/:userId", controllers.GetAUser())
-	router.PUT("/user/:userId", controllers.EditAUser())
-	router.DELETE("/user/:userId", controllers.DeleteAUser())
-	router.GET("/users", controllers.GetAllUsers())
+	router.POST("/add/users", controllers.CreateUser())
+	router.GET("/user/:id", controllers.GetAUser())
+	router.PUT("/user/:id", controllers.EditAUser())
+	router.DELETE("/delete/user/:id", controllers.DeleteAUser())
+	router.GET("/users/list", controllers.GetAllUsers())
 }
