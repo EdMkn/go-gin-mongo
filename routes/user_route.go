7d99e65182ae0c42ef5fa8761fe554a46cb330c3
EdMkn <nmanyim@rocketmail.com>
2023-02-27 12:17:23 +0100

    Endpoint suppression utilisateur

--- routes/user_route.go
+++ routes/user_route.go
@@ -10,4 +10,5 @@ func UserRoute(router *gin.Engine) {
 	//All routes related to users comes here
 	router.GET("/user/:userId", controllers.GetAUser())
 	router.PUT("/user/:userId", controllers.EditAUser())
+	router.DELETE("/user/:userId", controllers.DeleteAUser())
 }
