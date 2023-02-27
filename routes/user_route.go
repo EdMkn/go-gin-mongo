888e754d03cf224bc4198276bb92c5435fb47a29
EdMkn <nmanyim@rocketmail.com>
2023-02-27 12:14:16 +0100

    Endpoint Modification d'user

--- routes/user_route.go
+++ routes/user_route.go
@@ -8,5 +8,6 @@ import (
 
 func UserRoute(router *gin.Engine) {
 	//All routes related to users comes here
-	router.GET("/user/:userId", controllers.GetAUser()) //add this
+	router.GET("/user/:userId", controllers.GetAUser())
+	router.PUT("/user/:userId", controllers.EditAUser())
 }
